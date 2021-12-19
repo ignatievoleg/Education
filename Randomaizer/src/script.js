5f9c812174ada3aaf6894198cc367e28dc1466de
ignatievoleg <ignatievolegsky@gmail.com>
2021-12-19 19:13:24 +0200

    add BEM Randomaizer

--- Randomaizer/src/script.js
+++ Randomaizer/src/script.js
@@ -1,13 +1,11 @@
 let dataNumber = [];
 
-
 const inputFrom = document.querySelector('#from');
 const inputTo = document.querySelector('#to');
+let generateNumber = document.querySelector('#number');
 
-
-document.querySelector("#btn-generate").addEventListener('click', generate);
+document.querySelector('#btn-generate').addEventListener('click', generate);
 document.querySelector('#btn-reset').addEventListener('click', reset);
-
 
 function generate() {
     const from = Number(inputFrom.value);
@@ -15,7 +13,6 @@ function generate() {
     const range = to - from + 1;
 
     let num = Math.floor((Math.random() * range + from));
-
 
     while (dataNumber.includes(num)) {
         num++;
@@ -25,18 +22,23 @@ function generate() {
         }
     }
 
-
     dataNumber.push(num);
-    document.querySelector('#number').innerHTML = num;
+    generateNumber.innerHTML = 'Generated number: ' + num;
     if (dataNumber.length === range) {
         document.querySelector('#btn-generate').disabled = true;
+        generateNumber.innerHTML = 'Generated number: ' + num + ' Elements are over';
+    }
+
+    if (to <= from) {
+        alert('неправильное условие');
+        reset();
     }
 }
 
 function reset() {
     document.querySelector('#btn-generate').disabled = false;
-
     dataNumber = [];
     inputFrom.value = '';
     inputTo.value = '';
+    generateNumber.innerHTML = 'Generated number: ';
 }
